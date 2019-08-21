5c24bd73553884406ba8b0d23b171c786ede400d
mhairimccrindle@gmail.com <mhairimccrindle@gmail.com>
2019-08-21 19:42:36 +0100

    init of repo

---
+++ models/traveller.js
@@ -0,0 +1,51 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  const allStartLocations = this.journeys.map((journey) => {
+    return journey.startLocation;
+  })
+  return allStartLocations;
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  const allEndLocations = this.journeys.map((journey) => {
+    return journey.endLocation;
+  })
+  return allEndLocations;
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  const usedTransport = this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  })
+  return usedTransport;
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  const journeyDistance = this.journeys.filter((journey) => {
+    return journey.distance >= minDistance;
+  })
+  return journeyDistance;
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  const totalDistance = this.journeys.reduce((total, journey) => {
+    return total + journey.distance;
+  }, 0)
+  return totalDistance;
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  const transport = this.journeys.map((journey) => {
+    return journey.transport;
+  })
+  let uniqueTransport = transport.filter((modeOfTransport, index) => {
+    return transport.indexOf(modeOfTransport) === index;
+  });
+  return uniqueTransport;
+};
+
+
+module.exports = Traveller;
